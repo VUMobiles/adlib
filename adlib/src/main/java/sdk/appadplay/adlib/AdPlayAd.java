b458818e7112569f26e1831e224841726ed0ba01
VU Mobile Ltd <vumobile.celeb@gmail.com>
2018-06-06 11:14:30 +0600

    ad video

--- adlib/src/main/java/sdk/appadplay/adlib/AdPlayAd.java
+++ adlib/src/main/java/sdk/appadplay/adlib/AdPlayAd.java
@@ -1,14 +1,24 @@
 package sdk.appadplay.adlib;
 
+import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,13 +34,16 @@ public class AdPlayAd {
     private Context mContext;
     private RelativeLayout adLayout;
     private WebView mWebView;
+    static VideoView videoview;
 
     public AdPlayAd(Context context, RelativeLayout layout) {
         this.mContext = context;
         this.adLayout = layout;
     }
 
-    public void loadVideoAd(final String publisherID, final String packageName, final String requestType){
+    //==============================Interstitial Ad==================================//
+
+    public void loadInterstitial(final String publisherID, final String packageName, final String requestType){
 
         adLayout.setVisibility(View.GONE);
         new Handler().postDelayed(new Runnable() {
@@ -148,4 +161,248 @@ public class AdPlayAd {
         return divID;
     }
 
+    //*******************************END of interstitial ad Method*****************************//
+
+
+
+    //==============================Video Ad==================================//
+
+    public void loadVideoAd(final String myPublisherId) {
+
+        String UserAgent = (new WebView(mContext)).getSettings().getUserAgentString();
+        String userAgent = UserAgent.replaceAll(" ", "%20");
+        String bannerAdUrl = "https://adsapi.adplay-mobile.com/adplayapi?mode=sdk&pid=" + myPublisherId +
+                "&fp=2&useragent=" + userAgent + "&pos=63&request=video&response=json";
+        Log.d("BannerAdUrl", bannerAdUrl);
+
+//        JsonObjectRequest request = new JsonObjectRequest(bannerAdUrl, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//
+//
+//                try {
+//                    JSONObject mainObject = response.getJSONObject("seatbid");
+//                    JSONObject seatBidObj = mainObject.getJSONObject("bid");
+//                    String nurl = seatBidObj.getString("nurl");
+//                    String videoUrl = seatBidObj.getString("video_url");
+//
+//                    JSONObject objVideo = seatBidObj.getJSONObject("video");
+//                    String playMin = objVideo.getString("play_minutes");
+//                    String adRole = objVideo.getString("role");
+//                    String repeat = objVideo.getString("repeat");
+//
+//
+//
+//                    if (!nurl.equals(null) || !nurl.isEmpty() ) {
+//                        playVideo(context, nurl, videoUrl, playMin, adLayout,adRole,repeat);
+//                    }
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Log.d("Response", error.toString());
+//            }
+//        });
+//
+//        Volley.newRequestQueue(context).add(request);
+
+        new GetVideoAdProperties().execute(myPublisherId);
+
+    }
+
+    public class GetVideoAdProperties extends AsyncTask<String, Void, String> {
+
+        String UserAgent = (new WebView(mContext)).getSettings().getUserAgentString();
+        String userAgent = UserAgent.replaceAll(" ", "%20");
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String pubId = params[0];
+
+            String bannerAdUrl = "https://adsapi.adplay-mobile.com/adplayapi?mode=sdk&pid=" + pubId +
+                    "&fp=2&useragent=" + userAgent + "&pos=63&request=video&response=json";
+            Log.d("BannerAdUrl", bannerAdUrl);
+            HttpHandler sh = new HttpHandler();
+            String jsonString = sh.makeServiceCall(bannerAdUrl);
+
+
+            return jsonString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            try {
+                JSONObject obj = new JSONObject(result);
+                JSONObject mainObject = obj.getJSONObject("seatbid");
+                JSONObject seatBidObj = mainObject.getJSONObject("bid");
+                String nurl = seatBidObj.getString("nurl");
+                String videoUrl = seatBidObj.getString("video_url");
+
+                JSONObject objVideo = seatBidObj.getJSONObject("video");
+                String playMin = objVideo.getString("play_minutes");
+                String adRole = objVideo.getString("role");
+                String repeat = objVideo.getString("repeat");
+
+                if (!nurl.equals(null) || !nurl.isEmpty() ) {
+                    playVideo(mContext, nurl, videoUrl, playMin, adLayout,adRole,repeat);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @SuppressLint("ResourceType")
+    private void playVideo(final Context context, final String nurl, final String videoUrl, String playMin, final RelativeLayout adLayout, final String adRole, final String repeat) {
+
+        adLayout.setVisibility(View.GONE);
+
+        final RelativeLayout subLayout = new RelativeLayout(context);
+        final RelativeLayout.LayoutParams subLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+
+        final TextView txtTimeRemain = new TextView(context);
+        txtTimeRemain.setTextColor(Color.WHITE);
+        txtTimeRemain.setId(3);
+
+        int time;
+        int afterPlay = Integer.parseInt(playMin);
+
+        videoview = new VideoView(context);
+        videoview.setId(4);
+        videoview.setClickable(true);
+
+        final RelativeLayout.LayoutParams videoParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        videoParams.addRule(RelativeLayout.CENTER_VERTICAL);
+        videoParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+
+        final Button btnClose = new Button(context);
+        btnClose.setText("SKIP AD");
+        btnClose.setTextColor(Color.WHITE);
+        btnClose.setBackgroundResource(Color.parseColor("#00000000"));
+        btnClose.setId(2);
+
+        final RelativeLayout.LayoutParams txtParams = new RelativeLayout.LayoutParams(25,25);
+        txtParams.addRule(RelativeLayout.BELOW,videoview.getId());
+        txtParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        txtParams.setMargins(0, 0, 10,5);
+
+        final RelativeLayout.LayoutParams btnCloseParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        btnCloseParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+
+
+        if (adRole.equals("2")){
+            // video ad will play after 15 sec
+            time = 15000;
+        }else {
+            // video ad will play after 1 sec
+            time = 1000;
+        }
+
+        final Handler handler = new Handler();
+        final Runnable myRunnable = new Runnable() {
+            @Override
+            public void run() {
+                adLayout.setVisibility(View.VISIBLE);
+                adLayout.setBackgroundColor(Color.BLACK);
+                btnClose.setVisibility(View.GONE);
+
+                videoview.setVideoURI(Uri.parse(videoUrl));
+                videoview.requestFocus();
+                videoview.start();
+                videoview.pause();
+
+                videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mediaPlayer) {
+                        try{
+                            Log.d("VideoAd","Prepare");
+                            videoview.start();
+                            Log.d("VideoAd","Start");
+                            if (videoview.isPlaying()){
+                                Log.d("VideoAd","Playing");
+                                new CountDownTimer(15000, 1000) {
+
+                                    public void onTick(long millisUntilFinished) {
+                                        double remian = millisUntilFinished / 1000;
+                                        int remainTime = (int) remian;
+                                        String s = String.valueOf(remainTime);
+                                        txtTimeRemain.setText(s);
+                                        Log.d("RemainTime",s);
+                                    }
+
+                                    public void onFinish() {
+                                        btnClose.setVisibility(View.VISIBLE);
+                                        txtTimeRemain.setVisibility(View.GONE);
+                                    }
+
+                                }.start();
+
+                            }
+                        }catch (NullPointerException e){
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        };
+        handler.postDelayed(myRunnable, afterPlay * 1000);
+
+        subLayout.addView(txtTimeRemain,txtParams);
+        subLayout.addView(btnClose, btnCloseParams);
+        subLayout.addView(videoview, videoParams);
+        adLayout.addView(subLayout, subLayoutParams);
+
+
+        videoview.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Log.d("vclick", "click");
+
+                videoview.stopPlayback();
+                adLayout.setVisibility(View.GONE);
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(nurl));
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(intent);
+                } catch (ActivityNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                return false;
+            }
+        });
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (videoview.isPlaying()) {
+                    videoview.stopPlayback();
+                    adLayout.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        videoview.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                adLayout.setVisibility(View.GONE);
+                Log.d("VideoAd","Finish");
+            }
+        });
+    }
+
+    //*******************************END of interstitial ad Method*****************************//
 }
