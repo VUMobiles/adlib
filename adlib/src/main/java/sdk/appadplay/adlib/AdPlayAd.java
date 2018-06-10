4d96e62aaa447a27ab15839ac3d881e53ebc3e08
VU Mobile Ltd <vumobile.celeb@gmail.com>
2018-06-10 12:29:48 +0600

    back to pavilion

--- adlib/src/main/java/sdk/appadplay/adlib/AdPlayAd.java
+++ adlib/src/main/java/sdk/appadplay/adlib/AdPlayAd.java
@@ -35,22 +35,11 @@ public class AdPlayAd {
     private RelativeLayout adLayout;
     private WebView mWebView;
     static VideoView videoview;
-    public static final String PRE_ROLE_VIDEO = "1";
-    public static final String FULL_SCREEN = "2";
-    public int height;
 
     public AdPlayAd(Context context, RelativeLayout layout) {
         this.mContext = context;
         this.adLayout = layout;
     }
-
-    public AdPlayAd(Context context, RelativeLayout layout,int height) {
-        this.mContext = context;
-        this.adLayout = layout;
-        this.height = height;
-    }
-
-
 
     //==============================Interstitial Ad==================================//
 
@@ -178,7 +167,7 @@ public class AdPlayAd {
 
     //==============================Video Ad==================================//
 
-    public void loadVideoAd(final String myPublisherId,VideoAdCallBack videoAdCallBack, String adType) {
+    public void loadVideoAd(final String myPublisherId,VideoAdCallBack videoAdCallBack) {
 
         this.videoAdCallBackStart = videoAdCallBack;
 
@@ -224,12 +213,7 @@ public class AdPlayAd {
 //
 //        Volley.newRequestQueue(context).add(request);
 
-        if (adType.equals(PRE_ROLE_VIDEO)){
-            new GetVideoAdProperties().execute(myPublisherId);
-        }else if (adType.equals(FULL_SCREEN)){
-            new GetVideoAdPropertiesPreroleAd().execute(myPublisherId);
-        }
-
+        new GetVideoAdProperties().execute(myPublisherId);
 
     }
 
@@ -281,223 +265,6 @@ public class AdPlayAd {
                 e.printStackTrace();
             }
         }
-    }
-
-    public class GetVideoAdPropertiesPreroleAd extends AsyncTask<String, Void, String> {
-
-        String UserAgent = (new WebView(mContext)).getSettings().getUserAgentString();
-        String userAgent = UserAgent.replaceAll(" ", "%20");
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            String pubId = params[0];
-
-            String bannerAdUrl = "https://adsapi.adplay-mobile.com/adplayapi?mode=sdk&pid=" + pubId +
-                    "&fp=2&useragent=" + userAgent + "&pos=63&request=video&response=json";
-            Log.d("BannerAdUrl", bannerAdUrl);
-            HttpHandler sh = new HttpHandler();
-            String jsonString = sh.makeServiceCall(bannerAdUrl);
-
-
-            return jsonString;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-
-            try {
-                JSONObject obj = new JSONObject(result);
-                JSONObject mainObject = obj.getJSONObject("seatbid");
-                JSONObject seatBidObj = mainObject.getJSONObject("bid");
-                String nurl = seatBidObj.getString("nurl");
-                String videoUrl = seatBidObj.getString("video_url");
-
-                JSONObject objVideo = seatBidObj.getJSONObject("video");
-                String playMin = objVideo.getString("play_minutes");
-                String adRole = objVideo.getString("role");
-                String repeat = objVideo.getString("repeat");
-
-                if (!nurl.equals(null) || !nurl.isEmpty() ) {
-                    playVideoPreRole(mContext, nurl, videoUrl, playMin, adLayout,adRole,repeat);
-                }
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    @SuppressLint("ResourceType")
-    private void playVideoPreRole(final Context mContext, final String nurl, final String videoUrl, String playMin, final RelativeLayout adLayout, String adRole, String repeat) {
-
-
-        adLayout.setVisibility(View.GONE);
-
-        final RelativeLayout subLayout = new RelativeLayout(mContext);
-        final RelativeLayout.LayoutParams subLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, height);
-
-        final TextView txtTimeRemain = new TextView(mContext);
-        txtTimeRemain.setTextColor(Color.WHITE);
-        txtTimeRemain.setId(3);
-
-        int time;
-        int afterPlay = Integer.parseInt(playMin);
-
-        videoview = new VideoView(mContext);
-        videoview.setId(4);
-        videoview.setClickable(true);
-        videoview.requestFocus();
-        videoview.setZOrderOnTop(true);
-
-
-        final RelativeLayout.LayoutParams videoParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        videoParams.addRule(RelativeLayout.CENTER_VERTICAL);
-        videoParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
-        videoParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-        videoParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-        videoParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-
-
-        final Button btnClose = new Button(mContext);
-        btnClose.setText("SKIP AD");
-        btnClose.setTextColor(Color.WHITE);
-        btnClose.setBackgroundResource(Color.parseColor("#00000000"));
-        btnClose.setId(2);
-
-        final RelativeLayout.LayoutParams txtParams = new RelativeLayout.LayoutParams(25,25);
-//        txtParams.addRule(RelativeLayout.BELOW,videoview.getId());
-        txtParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-       // txtParams.setMargins(0, 0, 10,10);
-
-        final RelativeLayout.LayoutParams btnCloseParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-        btnCloseParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-
-
-        if (adRole.equals("2")){
-            // video ad will play after 15 sec
-            time = 15000;
-        }else {
-            // video ad will play after 1 sec
-            time = 1000;
-        }
-        videoview.setVideoURI(Uri.parse(videoUrl));
-
-        final Handler handler = new Handler();
-        final Runnable myRunnable = new Runnable() {
-            @Override
-            public void run() {
-                adLayout.setVisibility(View.VISIBLE);
-                adLayout.setBackgroundColor(Color.BLACK);
-                btnClose.setVisibility(View.GONE);
-
-                //videoview.requestFocus();
-
-                if (videoAdCallBackStart!=null){
-                    videoAdCallBackStart.isPlayingVideoAD(true);
-                }
-                videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                    @Override
-                    public void onPrepared(MediaPlayer mediaPlayer) {
-                        try{
-                            Log.d("VideoAd","Prepare");
-                            videoview.start();
-                            Log.d("VideoAd","Start");
-                            if (videoview.isPlaying()){
-                                Log.d("VideoAd","Playing");
-                                new CountDownTimer(5000, 1000) {
-                                    public void onTick(long millisUntilFinished) {
-                                        double remian = millisUntilFinished / 1000;
-                                        int remainTime = (int) remian;
-                                        String s = String.valueOf(remainTime);
-                                        txtTimeRemain.setText(s);
-                                        Log.d("RemainTime",s);
-                                    }
-
-                                    public void onFinish() {
-                                        btnClose.setVisibility(View.VISIBLE);
-                                        txtTimeRemain.setVisibility(View.GONE);
-                                    }
-
-                                }.start();
-
-                            }
-                        }catch (NullPointerException e){
-                            e.printStackTrace();
-                        }
-                    }
-                });
-            }
-        };
-        handler.postDelayed(myRunnable, afterPlay * 1000);
-
-        subLayout.addView(txtTimeRemain,txtParams);
-        subLayout.addView(btnClose, btnCloseParams);
-        subLayout.addView(videoview, videoParams);
-        adLayout.addView(subLayout, subLayoutParams);
-
-
-        videoview.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                Log.d("vclick", "click");
-
-                try{
-                    videoview.pause();
-                    videoview.stopPlayback();
-                    adLayout.setVisibility(View.GONE);
-                    videoview = null;
-                    videoview.setVisibility(View.GONE);
-                }catch (NullPointerException e){
-                    e.printStackTrace();
-                }
-
-                try {
-                    handler.removeCallbacks(myRunnable);
-                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(nurl));
-//                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    mContext.startActivity(intent);
-                } catch (ActivityNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                return false;
-            }
-        });
-
-        btnClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (videoview.isPlaying()) {
-                    videoview.stopPlayback();
-                    videoview.setVisibility(View.GONE);
-                    adLayout.setVisibility(View.GONE);
-                    handler.removeCallbacks(myRunnable);
-                    if (videoAdCallBackStart!=null){
-                        videoAdCallBackStart.skipVideoAd(true);
-                    }
-                }
-            }
-        });
-
-        videoview.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                adLayout.setVisibility(View.GONE);
-                videoview.setVisibility(View.GONE);
-                Log.d("VideoAd","Finish");
-                handler.removeCallbacks(myRunnable);
-                if (videoAdCallBackStart!=null){
-                    videoAdCallBackStart.finishVideoAd(true);
-                }
-            }
-        });
-
     }
 
     @SuppressLint({"ResourceType", "ClickableViewAccessibility"})
